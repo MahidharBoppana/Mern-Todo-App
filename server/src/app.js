199261fcf363f523be5edfa726cea5beb1955ffc
Boppana Naga Venkata Mahidhar Babu <fswd2004@gmail.com>
2026-05-17 10:33:46 +0530

    changed origin

--- server/src/app.js
+++ server/src/app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://mern-todo-app-peach.vercel.app/",
     method: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   }),
